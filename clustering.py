eadbd1e75076bb31f9eca5071c3617f4232043ad
Alexander Rodriguez <rodriguez.al183@gmail.com>
2023-05-13 13:49:28 -0700

    completed clustering.py

--- clustering.py
+++ clustering.py
@@ -1,44 +1,66 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Alexander Rodriguez 
+# FILENAME: clustering.py
+# SPECIFICATION: Runs k-means multiple times and check which k value max the Silhouette coefficient
 # FOR: CS 4210- Assignment #5
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 20min
 #-----------------------------------------------------------*/
 
 #importing some Python libraries
 from sklearn.cluster import KMeans
-from sklearn.cluster import AgglomerativeClustering
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import silhouette_score
 from sklearn import metrics
 
-df = pd.read_csv('training_data.csv', sep=',', header=None) #reading the data by using Pandas library
+# Read the training data from 'training_data.csv'
+df = pd.read_csv('training_data.csv', sep=',', header=None)
 
-#assign your training data to X_training feature matrix
+# Assign your training data to X_training feature matrix
+X_training = df.values
 
-#run kmeans testing different k values from 2 until 20 clusters
-     #Use:  kmeans = KMeans(n_clusters=k, random_state=0)
-     #      kmeans.fit(X_training)
-     #--> add your Python code
+# Initialize variables to store the best k and silhouette coefficient
+best_k = 0
+best_silhouette = -1
 
+# Lists to store k values and their corresponding silhouette coefficients
+k_values = []
+silhouette_coeffs = []
 
-     #for each k, calculate the silhouette_coefficient by using: silhouette_score(X_training, kmeans.labels_)
-     #find which k maximizes the silhouette_coefficient
-     #--> add your Python code here
+# Run k-means testing different k values from 2 until 20 clusters
+for k in range(2, 21):
+    kmeans = KMeans(n_clusters=k, random_state=0)
+    kmeans.fit(X_training)
 
-#plot the value of the silhouette_coefficient for each k value of kmeans so that we can see the best k
-#--> add your Python code here
+    # Calculate the silhouette coefficient for the current k
+    silhouette = silhouette_score(X_training, kmeans.labels_)
 
-#reading the test data (clusters) by using Pandas library
-#--> add your Python code here
+    # Store the k and its corresponding silhouette coefficient
+    k_values.append(k)
+    silhouette_coeffs.append(silhouette)
 
-#assign your data labels to vector labels (you might need to reshape the row vector to a column vector)
-# do this: np.array(df.values).reshape(1,<number of samples>)[0]
-#--> add your Python code here
+    # Check if the current k has a higher silhouette coefficient than the previous best
+    if silhouette > best_silhouette:
+        best_k = k
+        best_silhouette = silhouette
 
-#Calculate and print the Homogeneity of this kmeans clustering
+# Plot the silhouette coefficient for each k value
+plt.plot(k_values, silhouette_coeffs, marker='o')
+plt.xlabel('Number of Clusters (k)')
+plt.ylabel('Silhouette Coefficient')
+plt.title('Silhouette Coefficient for Different k Values')
+plt.show()
+
+# Read the test data (clusters) from 'testing_data.csv'
+test_df = pd.read_csv('testing_data.csv', sep=',', header=None)
+
+# Assign your data labels to vector labels
+labels = np.array(test_df.values).reshape(1, -1)[0]
+
+# Run k-means with the best k value on the training data
+kmeans = KMeans(n_clusters=best_k, random_state=0)
+kmeans.fit(X_training)
+
+# Calculate and print the Homogeneity of this k-means clustering
 print("K-Means Homogeneity Score = " + metrics.homogeneity_score(labels, kmeans.labels_).__str__())
-#--> add your Python code here
